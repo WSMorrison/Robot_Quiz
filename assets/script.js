fb0e0b023dce805d38d609d95cc5b5bb33d040ca
WSMorrison <willimo@gmail.com>
2022-10-12 10:12:04 +0000

    Initial commit, with basic HTML structure, dagnostic CSS, diagnostic JS, and enumeration of intendended JS functions.

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+console.log ('JavaScript has been called correcly.');
+
+/*
+
+// Global functions
+function goGetQuestions () {
+
+}
+
+function calculateWhoIsAnswering () {
+
+}
+
+// User functions
+function displayUserQuestion () {
+
+}
+
+function retrieveUserAnswer () {
+
+}
+
+function userCheckAnswer () {
+
+}
+
+function displayCorrectAnswer () {
+
+}
+
+function calculateUserCorrect () {
+
+}
+
+function updateUserScore () {
+
+}
+
+
+// Robot functions
+function displayRobotQuestion () {
+
+}
+
+function robotHasToThinkAboutIt () {
+
+}
+
+function calculateRobotAnswer () {
+
+}
+
+/* function userCheckAnswer () {
+
+}
+
+function displayCorrectAnswer () {
+
+}*/ /*
+
+function updateRobotScore () {
+
+}
+
+function calculateWinner () {
+
+}
+
+function calculateIfTieBreaker () {
+
+}
+
+function showWinner () {
+    
+}
+
+*/
