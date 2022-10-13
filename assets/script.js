7e6bb4f3d044342441758a059a1fdade76413036
WSMorrison <willimo@gmail.com>
2022-10-13 15:56:41 +0000

    Add futher testing and diagnostic to imported API data in preparation for building quiz functions.

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,22 @@ console.log ('JavaScript file has been called sucessfully.');
 
 // Global variables
 
-let questionNumber = 1;
+let questionNumber = 0;
 let apiUrl = 'https://opentdb.com/api.php?amount=11&category=9&difficulty=easy&type=multiple';
 
 // Global functions
 
 async function goGetApi() {
     let apiData = await fetch(apiUrl);
-    let questionsRetrieved = await apiData.json();
+    let questions = await apiData.json();
     console.log('Questions have been retrieved successfully.');
-    console.log(questionsRetrieved);
-    console.log(questionsRetrieved.results[1].question);
+    console.log(questions);
+    console.log(questions.results[questionNumber].question);
+    console.log(questions.results[questionNumber].correct_answer);
+    console.log(questions.results[questionNumber].incorrect_answers[0]);
+    console.log(questions.results[questionNumber].incorrect_answers[1]);
+    console.log(questions.results[questionNumber].incorrect_answers[2]);
+    console.log('You did it you magnificent bastard.');
 }
 
 goGetApi();
